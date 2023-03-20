2a06f9146091a00d78452730b0f0b340aa567028
Louis Wijaya <70441261+louis-not@users.noreply.github.com>
2023-03-20 17:09:00 +0700

    Initial Commit

---
+++ Kudos-Cookies/Kudos-Cookies/Kudos_CookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kudos_CookiesApp.swift
+//  Kudos-Cookies
+//
+//  Created by Louis Mayco Dillon Wijaya on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Kudos_CookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
